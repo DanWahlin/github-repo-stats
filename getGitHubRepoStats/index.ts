41cf0dd853ae7acb274915f70aea1c9101cd3bfc
Dan Wahlin <you@example.com>
2022-09-25 10:40:28 -0700

    Add logging

--- getGitHubRepoStats/index.ts
+++ getGitHubRepoStats/index.ts
@@ -3,6 +3,7 @@ import { getStats } from './getStats';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a GitHub repo stats request.');
+    context.log(process.env['GITHUB_REPOS']);
     const stats = await getStats();
     console.log("The stats", stats);
     context.res = {
